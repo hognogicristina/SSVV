174f1c1f24457aa90dfc2ab8f7809aac6725bef8
Cristina Hognogi <90508610+hognogicristina@users.noreply.github.com>
2024-03-25 00:33:03 +0200

    add test cases

--- src/test/java/service/ServiceTest.java
+++ src/test/java/service/ServiceTest.java
@@ -8,13 +8,38 @@ import validation.Validator;
 
 public class ServiceTest extends TestCase {
 
+    private Validator<Student> studentValidator;
+    private StudentXMLRepository fileRepository1;
+    private Service service;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        studentValidator = new StudentValidator();
+        fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        service = new Service(fileRepository1, null, null);
+    }
+
     public void testSaveStudent() {
-        Validator<Student> studentValidator = new StudentValidator();
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        Service service = new Service(fileRepository1, null, null);
-
         assertEquals(1, service.saveStudent("-1", "nume", 1));
-
         assertEquals(0, service.saveStudent("1", "test", 123));
     }
+
+    public void testSaveStudentSuccess() {
+        assertEquals(0, service.saveStudent("2", "Alice", 234));
+    }
+
+    public void testSaveStudentWithEmptyId() {
+        assertEquals(1, service.saveStudent("", "Bob", 355));
+    }
+
+    public void testSaveStudentWithNullId() {
+        assertEquals(1, service.saveStudent(null, "Charlie", 444));
+    }
+
+    public void testSaveStudentWithDuplicateId() {
+        assertEquals(1, service.saveStudent("111", "qwe", 535));
+        assertEquals(0, service.saveStudent("111", "qwe", 535));
+    }
+
 }
